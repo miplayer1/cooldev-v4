c23f55bc3fb8107e4b4bab21b6962fbdf1c7b2a4
Letitbe133 <lionel.lft@me.com>
2018-03-16 21:31:57 +0100

    modif timing animation

--- js/scrollMagic-anim.js
+++ js/scrollMagic-anim.js
@@ -6,12 +6,12 @@ var page = window.location.href;
 if (page === "http://localhost/cooldev-v4/works.php") {
   // // define movement of panels
     var wipeAnimation = new TimelineMax()
-        wipeAnimation.fromTo("section.panelWorks.two", 3, {x: "-100%", opacity: 0.5}, {x: "0%", opacity: 1, ease: Linear.easeNone}, "+=10")  // in from left
-        wipeAnimation.fromTo("section.panelWorks.three", 3, {x: "100%", opacity: 0.5}, {x: "0%", opacity: 1, ease: Linear.easeNone}, "+=10")  // in from right
-        wipeAnimation.fromTo("section.panelWorks.four", 3, {x: "-100%", opacity: 0.5}, {x: "0%", opacity: 1, ease: Linear.easeNone}, "+=10") // in from top
-        wipeAnimation.fromTo("section.panelWorks.five", 3, {x: "100%", opacity: 0.5}, {x: "0%", opacity: 1, ease: Linear.easeNone}, "+=10") // in from top
-        wipeAnimation.fromTo("section.panelWorks.six", 3, {x: "-100%", opacity: 0.5}, {x: "0%", opacity: 1, ease: Linear.easeNone}, "+=10") // in from top
-        wipeAnimation.fromTo("section.panelWorks.seven", 3, {x: "100%", opacity: 0.5}, {x: "0%", opacity: 1, ease: Linear.easeNone}, "+=10"); // in from top
+        wipeAnimation.fromTo("section.panelWorks.two", 3, {x: "-100%", opacity: 0.5}, {x: "0%", opacity: 1, ease: Linear.easeNone}, "+=7")  // in from left
+        wipeAnimation.fromTo("section.panelWorks.three", 3, {x: "100%", opacity: 0.5}, {x: "0%", opacity: 1, ease: Linear.easeNone}, "+=7")  // in from right
+        wipeAnimation.fromTo("section.panelWorks.four", 3, {x: "-100%", opacity: 0.5}, {x: "0%", opacity: 1, ease: Linear.easeNone}, "+=7") // in from top
+        wipeAnimation.fromTo("section.panelWorks.five", 3, {x: "100%", opacity: 0.5}, {x: "0%", opacity: 1, ease: Linear.easeNone}, "+=7") // in from top
+        wipeAnimation.fromTo("section.panelWorks.six", 3, {x: "-100%", opacity: 0.5}, {x: "0%", opacity: 1, ease: Linear.easeNone}, "+=7") // in from top
+        wipeAnimation.fromTo("section.panelWorks.seven", 3, {x: "100%", opacity: 0.5}, {x: "0%", opacity: 1, ease: Linear.easeNone}, "+=7"); // in from top
 
     // // create scene to pin and link animation
     new ScrollMagic.Scene({
--- works.php
+++ works.php
@@ -50,6 +50,6 @@
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js" ></script>
   <script src="js/scrollMagic.js"></script>
-  <script src="js/scrollMagic-anim.min.js" ></script>
+  <script src="js/scrollMagic-anim.min.js"></script>
   <script src="js/main.min.js"></script>
 </body>
