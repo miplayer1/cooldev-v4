028cd3e64b8666ccb05a7013b2fc9f276bdfde9b
Letitbe133 <lionel.lft@me.com>
2018-03-18 14:50:24 +0100

    modif cdn normalize

--- mentions-legales.php
+++ mentions-legales.php
@@ -15,7 +15,7 @@
     <meta name="viewport" content="width=device-width initial-scale=1" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
     <link href="https://fonts.googleapis.com/css?family=Comfortaa%7cRoboto" rel="stylesheet">
-    <link rel="stylesheet" href="https://yarnpkg.com/en/package/normalize.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css" />
     <link rel="stylesheet" href="css/main.css" />
     <link rel="Shortcut icon" href="favicon.ico" type="image/x-icon" />
